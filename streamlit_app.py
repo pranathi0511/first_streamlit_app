3373c0d1d73358b04f849cd981215cdfafae9394
Pranathi Adabala <31755606+pranathi0511@users.noreply.github.com>
2023-08-02 19:39:32 +0530

    Update streamlit_app.py
    added header & text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Dinner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Orange")
+streamlit.text("Apple")
+streamlit.text("Mango")
