3e8f7ee4223da18dff2074dc3165a1f49b97d1b0
Pranathi Adabala <31755606+pranathi0511@users.noreply.github.com>
2023-08-04 21:33:44 +0530

    Update streamlit_app.py
    add json()

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
 streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
